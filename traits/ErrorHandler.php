01b9958dddc58cfb783b6d74ecf557a1f602f21b
Noor Abdi <57824560+noorfarooqy@users.noreply.github.com>
2023-01-04 17:29:44 +0300

    update error passage bug

--- traits/ErrorHandler.php
+++ traits/ErrorHandler.php
@@ -9,20 +9,22 @@ trait ErrorHandler
     private $status = 200;
     private $error_code = 0;
     private $success_message = '';
+    private $other_errors = [];
     public function setError(string $error, $error_code = '_001', $error_descriptions = [])
     {
         $this->error_message = $error;
         $this->error_code = $error_code;
-        $this->error_description = $this->getErrorDescription($error_descriptions);
+        $this->other_errors = $error_descriptions;
+        $this->error_description = $this->getErrorDescription();
     }
     public function setStatus(int $status)
     {
         $this->status = $status;
     }
 
-    public function getErrorDescription($error_descriptions = [])
+    public function getErrorDescription()
     {
-        return ErrorCodes::getErrorDescription($this->error_code, $error_descriptions);
+        return ErrorCodes::getErrorDescription($this->error_code, $this->other_errors);
     }
     public function getErrorCode()
     {
