0ab94196aafe43708f98e6910fb4cc12dcbb74c5
Noor Abdi <57824560+noorfarooqy@users.noreply.github.com>
2023-01-04 11:07:36 +0300

    update the error codes to contain errors from the system

--- contracts/ErrorCodes.php
+++ contracts/ErrorCodes.php
@@ -5,13 +5,13 @@ enum ErrorCodes:string {
     case _001 = 'Default Error description';
     case _002 = 'SMS - Invalid format number ';
 
-    public static function getErrorDescription($error)
+    public static function getErrorDescription($error, $other_errors =[])
     {
         if (!$error) {
             return '';
         }
 
-        $cases = static::cases();
+        $cases = count($other_errors) > 0 ? array_merge($other_errors, static::cases()) : static::cases();
         foreach ($cases as $key => $case) {
             if ($case->name == $error) {
                 return $case->value;
