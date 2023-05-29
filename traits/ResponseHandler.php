bc8c6ebcea09a97da526f94b3e068c2ef31e5b20
Noor Abdi <57824560+noorfarooqy@users.noreply.github.com>
2023-05-29 15:38:28 +0300

    Update ResponseHandler.php

--- traits/ResponseHandler.php
+++ traits/ResponseHandler.php
@@ -6,14 +6,19 @@ trait ResponseHandler
     use ErrorHandler;
     public function getResponse($data = []) : object
     {
-        return response()->json([
+        return response()->json($this->getPlainResponse($data), $this->getStatus());
+
+    }
+    public function getPlainResponse($data = []) : object
+    {
+        return [
             'error_code' => $this->getErrorCode(),
             'error_message' => $this->getMessage(),
             'error_description' => $this->getErrorDescription(),
             'status' => $this->getStatus(),
             'success_message' => $this->getSuccessMessage(),
             'data' => $data,
-        ], $this->getStatus());
+        ];
 
     }
 
